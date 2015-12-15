6435bf6e613cfcb693448a5fcd42885a19e303da
B Hicks <b@audiblePi.com>
2015-12-15 12:59:06 -0500

    updated registration page validation errors

--- main.php
+++ main.php
@@ -38,6 +38,20 @@ function fcc_load_scripts(){
     wp_enqueue_script( 'tablesorter', '/wp-content/plugins/fcc-dashboard/js/jquery.tablesorter.min.js' );
 }
 
+
+add_filter( 'registration_errors', 'tml_registration_errors' );
+function tml_registration_errors( $errors ) {
+    if ( empty( $_POST['first_name'] ) )
+        $errors->add( 'empty_first_name', '<strong>ERROR</strong>: Please enter your first name.' );
+    if ( empty( $_POST['last_name'] ) )
+        $errors->add( 'empty_last_name', '<strong>ERROR</strong>: Please enter your last name.' );
+    if ( empty( $_POST['university'] ) )
+        $errors->add( 'empty_university', '<strong>ERROR</strong>: Please enter your university.' );
+    if ( empty( $_POST['user_title'] ) )
+        $errors->add( 'empty_title', '<strong>ERROR</strong>: Please enter your title.' );
+    return $errors;
+}
+
 add_action( 'user_register', 'tml_user_register' );
 function tml_user_register( $user_id ) {
     if ( !empty( $_POST['first_name'] ) )
@@ -46,8 +60,8 @@ function tml_user_register( $user_id ) {
         update_user_meta( $user_id, 'last_name', $_POST['last_name'] );
     if ( !empty( $_POST['university'] ) )
         update_user_meta( $user_id, 'university', $_POST['university'] );
-    if ( !empty( $_POST['title'] ) )
-        update_user_meta( $user_id, 'title', $_POST['title'] );
+    if ( !empty( $_POST['user_title'] ) )
+        update_user_meta( $user_id, 'user_title', $_POST['user_title'] );
 }
 
 add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
@@ -56,7 +70,7 @@ function my_save_extra_profile_fields( $user_id ) {
     if ( !current_user_can( 'edit_user', $user_id ) )
         return false;
     update_usermeta($user_id, 'university', ( isset($_POST['university']) ? $_POST['university'] : '' ) );
-    update_usermeta($user_id, 'title', ( isset($_POST['title']) ? $_POST['title'] : '' ) );
+    update_usermeta($user_id, 'user_title', ( isset($_POST['user_title']) ? $_POST['user_title'] : '' ) );
 }
 
 add_action( 'profile_update', 'custom_profile_redirect', 12 );
@@ -76,9 +90,9 @@ function my_show_extra_profile_fields( $user ) { ?>
             </td>
         </tr>
         <tr>
-            <th><label for="title">Title</label></th>
+            <th><label for="user_title">Title</label></th>
             <td>
-                <input type="text" name="title" id="title" value="<?php echo esc_attr( get_the_author_meta( 'title', $user->ID ) ); ?>" class="regular-text" /><br />
+                <input type="text" name="user_title" id="user_title" value="<?php echo esc_attr( get_the_author_meta( 'user_title', $user->ID ) ); ?>" class="regular-text" /><br />
             </td>
         </tr>
     </table>
